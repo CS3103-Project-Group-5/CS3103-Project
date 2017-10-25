d61b69d2ba80b5e733bec4b0514ebc2f3a6b8f11
Qin_Shihuang <cafeboss@live.cn>
2017-10-25 15:02:54 +0800

    P2PClient

--- P2PClient.java
+++ P2PClient.java
@@ -1,7 +1,47 @@
 import java.util.*;
 
-class P2PClient {
+public class P2PClient {
 
-	
+	private String mode;
+	private String fileName;
+	private ArrayList<PeerInfo> peerList;
+
+	public static void main(String[] args) {
+		mode = args[0];
+		if (mode.equals("_list")) {
+			return Tracker.getFileList();
+		}
+
+		fileName = args[1];
+		System.out.println("Initiating " + mode + " for file " + fileName);
+
+		peerList = Tracker.getPeerList();
+	}
+}
+
+class Tracker {
+
+	private static final String TRACKER_ADDRESS = "trackerIP";
+	private static final String TRACKER_PORT = "trackerPort";
+	private Socket socket;
+
+	private Tracker() {
+		socket = new Socket(TRACKER_ADDRESS, TRACKER_PORT);
+	}
+
+	private void send() {
+	}
+
+	private void receive() {
+	}
+
+	public static getPeerList() {
+	}
+
+	public static getFileList() {
+		Tracker tracker = new Tracker();
+
+		tracker.send(query);
+	}
 
 }
